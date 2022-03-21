c92ae81ba004a7f6c27e5872ff4cd897d9f32d22
liuxia <1285370080@qq.com>
2022-03-21 14:37:19 +0800

    demo

--- WebGL/src/demo2/README.md
+++ WebGL/src/demo2/README.md
@@ -2,3 +2,20 @@
 
 - Triangles.html：绘制一个三角形
 - index.html: 利用鼠标动态绘制三角形的功能
+
+主要涉及内容：
+
+- 三角形图元分类
+  - gl.TRIANGLES：基本三角形
+  - gl.TRIANGLE_STRIP：三角带
+  - gl.TRIANGLE_FAN：三角扇
+- 类型化数组的作用
+  - Float32Array：32位浮点数组
+- 使用缓冲区传递数据
+  - gl.createBuffer：创建buffer
+  - gl.bindBuffer：绑定某个缓冲区对象为当前缓冲区
+  - gl.bufferData：往缓冲区中复制数据
+  - gl.enableVertexAttribArray：启用顶点属性
+  - gl.vertexAttribPointer：设置顶点属性从缓冲区中读取数据的方式
+- 动态绘制三角形
+  - 改变顶点信息，然后通过缓冲区将改变后的顶点信息传递到着色器，重新绘制三角形
--- WebGL/src/demo3/index.js
+++ WebGL/src/demo3/index.js
@@ -7,7 +7,6 @@ let canvas = getCanvas('#webgl');
 resizeCanvas(canvas);
 //获取绘图上下文
 let gl = getContext(canvas);
-
 //创建顶点着色器
 var vertexShader = createShaderFromScript(gl, gl.VERTEX_SHADER,'vertexShader');
 //创建片元着色器
@@ -20,78 +19,61 @@ gl.useProgram(program);
 /**
  * 定义缓冲区，并将数据存缓冲区
  */
-// 定义三角形的顶点
+// 定义顶点
 var positions = [];
 // 找到顶点着色器中的变量 a_Position
 var a_Position = gl.getAttribLocation(program, 'a_Position')
+// 启用着色器中的属性 a_Position
+gl.enableVertexAttribArray(a_Position)
+
 // 找到顶点着色器中的变量 a_Screen_Size
 var a_Screen_Size = gl.getAttribLocation(program, 'a_Screen_Size')
-// 获得片元着色器的变量 u_Color
-var u_Color = gl.getUniformLocation(program, 'u_Color')
 // 为顶点着色器中的 a_Screen_Size, 传递canves 的宽高信息
 gl.vertexAttrib2f(a_Screen_Size, canvas.width, canvas.height)
+
+// 获得片元着色器的变量 u_Color
+var u_Color = gl.getUniformLocation(program, 'u_Color')
+// 为片元着色器中的 u_Color 传递颜色
+gl.uniform4f(u_Color, 255, 0, 0, 1)
 
 // 创建一个缓冲区
 var buffer = gl.createBuffer()
 // 绑定缓存区
 gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
 
-// 往当前缓冲区中写入数据
-// gl.STATIC_DRAW  提示 WebGL 我们不会频繁改变缓冲区中的数据, WebGL 会根据这个参数做一些优化处理
-// gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
-
 /**
  * 从缓冲区中提取数据
  */
-// 启用着色器中的属性 a_Position
-gl.enableVertexAttribArray(a_Position)
 
 // 设置从缓冲区提取数据的方式
-// 每次提取两个数据
-var size = 2
-// 每个数据的类型是32位浮点型
-var type = gl.FLOAT
-// 不需要归一化数据
-var normalize = false
-// 每次迭代运行需要移动数据数*每个数据所占用内存 到下一个数据开始点
-var stride = 0
-// 从缓冲起始位置开始读取
-var offset = 0
 // 将 a_Position 变量获取数据的缓冲区指向当前绑定的 buffer
-gl.vertexAttribPointer(a_Position, size, type, normalize, stride, offset)
+gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
 
 // 添加点击事件监听
 canvas.addEventListener('mouseup', e => {
   var x = e.pageX
   var y = e.pageY
   positions.push(x, y)
-  if(positions.length % 6 === 0 ){
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
-    // 为片元着色器中的 u_Color 传递颜色
-    gl.uniform4f(u_Color, 255, 0, 0, 1)
+  if(positions.length > 0 ){
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.DYNAMIC_DRAW)
     render(gl)    
   }
 })
 //设置清空画布颜色为黑色。
 gl.clearColor(0.0, 0.0, 0.0, 1.0);
-//用上一步设置的清空画布颜色清空画布。
+//用上一步设置的清空画布颜色清空画布
 gl.clear(gl.COLOR_BUFFER_BIT);
-
-
 
 /**
  * 绘制方法
  */
 function render(gl){
-    //设置清空画布颜色为黑色。
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     //用上一步设置的清空画布颜色清空画布。
     gl.clear(gl.COLOR_BUFFER_BIT);
     //绘制图元设置为三角形
-    var primitiveType = gl.TRIANGLES;
+    var primitiveType = gl.LINE_LOOP; // LINES：基本线段 / LINE_STRIP：带状线段 / LINE_LOOP：环状线段
     //从顶点数组的开始位置取顶点数据
     var drawOffset = 0;
-    //因为我们要绘制 N 个点，所以执行 N 次顶点绘制操作。
+    //因为我们要绘制 N 个点，所以执行 N 次顶点绘制操作
     gl.drawArrays(primitiveType, drawOffset, positions.length / 2);
-
 }
